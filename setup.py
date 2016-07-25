950ffa08186884b4e6f9322c25fb277fe3c4ca5f
Jenkins <jenkins@opennodecloud.com>
2016-07-25 22:04:03 +0000

    Preparing new release: 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name='nodeconductor-auth-valimo',
-    version='0.1.1',
+    version='0.1.2',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://nodeconductor.com',
